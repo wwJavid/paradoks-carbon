5d9666dd0b4679c2de0758bd5a691b01cf4cf37b
Sakina <sakinaibrahimova7@gmail.com>
2023-05-26 11:46:50 +0300

    build:utilities

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,12 @@
 import styles from "./header.module.scss";
 import Brand from "../brand/brand";
+import Utilities from "../utilities/utilities";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Brand />
+      <Utilities />
     </header>
   );
 }
