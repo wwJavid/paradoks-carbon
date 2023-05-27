817f9feb7c3897cf8abc7884beda26afe2142349
Sakina <sakinaibrahimova7@gmail.com>
2023-05-27 14:27:31 +0300

    fix: nav-comments

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -2,14 +2,14 @@ import styles from "./nav.module.scss";
 
 function Nav() {
   return (
-    <div className={styles.nav}>
+    <nav className={styles.nav}>
       <ul className={styles.nav_ul}>
         <li>ana səhifə</li>
         <li>məqalələr</li>
         <li>karta</li>
         <li>podkast</li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
