fd6f890991b991356c2717c7a6c9e8a6932ccb56
Sakina <sakinaibrahimova7@gmail.com>
2023-06-19 14:35:47 +0300

    build: nav-links

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,25 +1,31 @@
 import styles from "./nav.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 function Nav() {
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.nav_ul}>
-        <li>ana səhifə</li>
-        <li>məqalələr</li>
-        <li>karta</li>
-        <li>podkast</li>
-      </ul>
-      <button className={styles.menu}>
-        <Image
-          priority
-          src="/assets/menu.svg"
-          alt="menu"
-          width={32}
-          height={32}
-        />
-      </button>
-    </nav>
+    <>
+      <nav className={styles.nav}>
+        <ul className={styles.nav_ul}>
+          <li>
+            <Link href="/home">ana səhifə</Link>
+          </li>
+          <li>
+            <Link href="/articles">məqalələr</Link>
+          </li>
+          <li>
+            <Link href="/karta">karta</Link>
+          </li>
+          <li>
+            <Link href="/podcast">podkast</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.menu}>
+        <Image src="/assets/menu.svg" alt="menu" width={32} height={32} />
+      </div>
+    </>
   );
 }
 
---
+++ pages/podcast.tsx
@@ -0,0 +1,9 @@
+const Podcast = () => {
+  return (
+    <div>
+      <h1>Podkast</h1>
+    </div>
+  );
+};
+
+export default Podcast;
