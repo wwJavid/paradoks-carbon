69a14b92c67c72a22fb6d0198a204c894feafa5b
Sakina <sakinaibrahimova7@gmail.com>
2023-06-19 15:07:22 +0300

    build: menu

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,8 +1,15 @@
 import styles from "./nav.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
 
 function Nav() {
+  const [open, setOpen] = useState(false);
+
+  function handleClick() {
+    setOpen(!open);
+  }
+
   return (
     <>
       <nav className={styles.nav}>
@@ -22,9 +29,28 @@ function Nav() {
         </ul>
       </nav>
 
-      <div className={styles.menu}>
+      <div className={styles.menu} onClick={handleClick}>
         <Image src="/assets/menu.svg" alt="menu" width={32} height={32} />
       </div>
+
+      {open && (
+        <div className={styles.menu_items}>
+          <ul>
+            <li>
+              <Link href="/home">ana səhifə</Link>
+            </li>
+            <li>
+              <Link href="/articles">məqalələr</Link>
+            </li>
+            <li>
+              <Link href="/karta">karta</Link>
+            </li>
+            <li>
+              <Link href="/podcast">podkast</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
